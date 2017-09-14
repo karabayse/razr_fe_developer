05f23d5c3b3927859747ac35a787919867fc06b0
Kara Bayse <karabayse@gmail.com>
2017-09-14 16:18:32 -0500

    created skeletons for sort and generate functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -21,4 +21,20 @@ vm.shape = function() {
 }; // end shape function
 
 
+// sorting function
+// Create a method that accepts an array of Circles and Squares, and returns the
+// array sorted by area, in descending order
+vm.sort = function() {
+
+}; // end sort function
+
+
+// generate function
+// Create a method that generates an array of 50 squares of varying side length
+// between 1 and 100, and 50 circles of varying diameter between 1 and 100.
+// The sizes of each shape should be randomly generated.
+vm.generate = function() {
+
+}; // end generate function
+
 }); // end controller
