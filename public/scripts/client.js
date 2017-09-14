00cc725d20655e723c674f33840aeb95ea086f57
Kara Bayse <karabayse@gmail.com>
2017-09-14 16:04:48 -0500

    adjusted routes files and reduced button count from 2 to 1

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,5 +5,20 @@ console.log('in clients.js');
 var myApp = angular.module('myApp', ['ngRoute']);
 
 myApp.controller('ShapeController', function(ShapeService) {
+  console.log('in ShapeController');
+  var vm = this;
+
+// shape function
+vm.shape = function() {
+  var shapeObject = {
+    radius: vm.radiusInput,
+    area: vm.areaInput,
+    length: vm.lengthInput
+  }; // end shapeObject
+  console.log('shapeObject:', shapeObject);
+  ShapeService.shape(shapeObject).then(function(){
+});
+}; // end shape function
+
 
 }); // end controller
--- public/scripts/services/service.js
+++ public/scripts/services/service.js
@@ -3,7 +3,16 @@
 myApp.service('ShapeService', function($http) {
   var sv = this;
 
-
+// POST for shape
+sv.shape = function(shape) {
+  return $http({
+    method: 'POST',
+    url: '/shape',
+    data: shape
+  }).then(function(response) {
+    console.log('back from shape post:', response);
+  });
+}; // end shape 
 
 
 }); // end service
