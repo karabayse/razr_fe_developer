e69b6e6c647f52132989855406c4084f2f31766c
Kara Bayse <karabayse@gmail.com>
2017-09-14 15:16:59 -0500

    added get call for shape object

--- modules/routes/schema.js
+++ modules/routes/schema.js
@@ -12,5 +12,13 @@ var shapeSchema = new mongoose.Schema({
 
 var shapeModel = mongoose.model('shapeModel', shapeSchema);
 
+router.get('/', function(req, res) {
+  console.log('shapeObject');
+  shapeModel.find().then(function(shape) {
+    res.send(shape);
+    console.log('shape:', shape);
+  });
+}); // end shape get call
+
 
 module.exports = router;
